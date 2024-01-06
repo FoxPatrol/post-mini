afaff7faf8911fe0251ed86cf1d60c4bca2d1501
Jose Alves <jose.amiginho@hotmail.com>
2024-01-07 00:32:12 +0100

    create angular app

---
+++ src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(FormsModule), importProvidersFrom(HttpClientModule), provideAnimations()]
+};
