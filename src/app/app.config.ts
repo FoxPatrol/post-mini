c80fd320f4f33c4fa59abdce75b9c7cd7869958d
Jose Alves <jose.amiginho@hotmail.com>
2024-01-14 16:26:06 +0100

    add http request

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,11 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(FormsModule), importProvidersFrom(HttpClientModule), provideAnimations()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(FormsModule), importProvidersFrom(HttpClientModule), provideAnimations(), provideHttpClient(withFetch())]
 };
